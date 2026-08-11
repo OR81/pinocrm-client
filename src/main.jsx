1d3301dec434b171622b7b7677b518e837b67520
Aylin <aylin@Aylins-MacBook-Air.local>
2026-08-11 12:04:57 +0330

    update my section

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import './app.css'
+import './App.css'
 import App from './App.jsx'
 import { ThemeProvider } from './components/theme'
 
